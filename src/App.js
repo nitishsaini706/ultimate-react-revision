c01dfcdf6732b6c6b54a16584401d39306ebf077
nitishsaini706 <nitishsaini706@gmail.com>
2022-06-18 16:44:16 +0530

    why components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-const pet = ()=>{
-    return React.createElement("div",{},[
-            React.createElement("h1",{},"hi")
-        ])
-    
-}
+import React from 'react';
+// import ReactDom from 'react-dom';
+// this will increae speed as we're not import whole module but just the function we require
+import {render} from 'react-dom';
+import pet from "./Pet";
 
 const App = () => {
     // this function has to be fast as it has to render 
@@ -12,12 +11,21 @@ const App = () => {
         // {},this is id
         {},
         [
-            React.createElement(pet);
+            React.createElement(pet)
         ]
     );
 };
+// const pet = ()=>{
+//     return React.createElement("div",{},[
+//             React.createElement("h1",{},"hi")
+//         ])
+    
+// }
+
+// after installing parcel we can now bundle our modules together so we can create another component for pet
 
 // here it's creating instance of app and putting it in root , it renders and append it to dom of browser 
 // as browser cannot render react
 // this is pure react 
-ReactDom.renders(React.createElement(App), document.getElementById("root"));
+
+render(React.createElement(App), document.getElementById("root"));
---
+++ src/Pet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const pet = ()=>{
+        return React.createElement("div",{},[
+                React.createElement("h1",{},"hi")
+            ])
+        
+    }
+    
+export default pet;
+
+// by default export pet , as we need it in other module
