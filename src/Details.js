87df32f1f4826c22b836b6c9b5ec913d5166177b
nitishsaini706 <nitishsaini706@gmail.com>
2022-06-20 15:36:51 +0530

    class componenet

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,13 @@
 import { useParams } from "react-router-dom";
+import { Component } from "react";
 
-const Details = () =>{
+// const Details = () =>{
     // it's extracting id from parameters , just like in express
-    const {id} = useParams();
-return <h2>{id}</h2>
-};
+    // const {id} = useParams();
+// return <h2>{id}</h2>
+// };
+
+// example of class component 
+
 
 export default Details;
