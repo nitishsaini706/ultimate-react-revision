277365217097148d1b2568d63d1ce3d2eca5e89d
nitishsaini706 <nitishsaini706@gmail.com>
2022-06-20 16:12:02 +0530

    created carasoul

--- src/Carosoul.js
+++ src/Carosoul.js
@@ -5,17 +5,37 @@ class Carasoul extends Component{
     constructor(props)
     {
         super(props);
-        this.state = {active : }
+        this.state = {active :0 }
     }
 
     static defaultProps=  {
         images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
     };
+
+    render() {
+        const {active} = this.state;
+        const {images} = this.props;
+
+        return ( 
+            <div className="carosoul">
+                <img src={images[active]} alt="aimal"/>
+                <div className="carusoul-smaller">
+                    {images.map((photo,index) => {
+                        <img 
+                            key={photo}
+                            src={photo}
+                            className={index === active ? "active" : ""}
+                        />
+                    })}
+                </div>
+            </div>
+        )
+    }
 }
 
 
-const x = new Carasoul();
+// const x = new Carasoul();
 
-x.state.active;
+// x.state.active;
 
 export default Carasoul;
