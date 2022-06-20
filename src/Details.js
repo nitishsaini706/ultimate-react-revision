3e5219afdf4d80eec3ad5ce182d3371270d43f4b
nitishsaini706 <nitishsaini706@gmail.com>
2022-06-20 15:48:58 +0530

    state changing

--- src/Details.js
+++ src/Details.js
@@ -17,6 +17,20 @@ class Details extends Component {
         this.state = {loading : true}
     }
 
+    async componentDidMount()
+    {
+        const res = await fetch(
+            `http://pets-v2-dev-apis.com/pets?id=${this.props.params.id}`
+        );
+        const json = await res.json();
+        
+
+        // and like we use usestate in function we need to change the state of loading 
+
+        this.setState({
+            loading:false
+        })
+    }
     render()
     {
         if(this.state.loading)
@@ -33,7 +47,7 @@ class Details extends Component {
                 <h1>{name}</h1>
                 <h2>{animal} - {breed}</h2>
             </div>
-        )
+        );
     }
 }
 
