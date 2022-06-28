1e9e5d1851b06e7d4449b6b951ce3a01e8a76252
nitishsaini706 <nitishsaini706@gmail.com>
2022-06-28 14:55:51 +0530

    using lazy and code spliting

--- intermediate/intermediate/src/App.js
+++ intermediate/intermediate/src/App.js
@@ -2,14 +2,23 @@ import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 import { StrictMode, useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Details from "./Details";
+// import Details from "./Details";
 import ThemeContext from "./ThemeContext";
+
+// import from React
+import { useState, StrictMode, lazy, Suspense } from "react";
+// delete Details & Search params imports
+
+// above const App =
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 
 const App = () => {
   const theme = useState("darkblue");
   return (
     <StrictMode>
+      <Suspense fallback={<h1>loading route …</h1>}>
       <ThemeContext.Provider value={theme}>
         <BrowserRouter>
           <header className=" mb-10">
@@ -21,6 +30,7 @@ const App = () => {
           </Routes>
         </BrowserRouter>
       </ThemeContext.Provider>
+      </Suspense>
     </StrictMode>
   );
 };
