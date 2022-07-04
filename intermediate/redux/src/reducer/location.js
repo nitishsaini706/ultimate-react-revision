2216cf9379a796d590cb4eab375e081b879ab298
nitishsaini706 <nitishsaini706@gmail.com>
2022-07-04 12:45:37 +0530

    location reducer

---
+++ intermediate/redux/src/reducer/location.js
@@ -0,0 +1,11 @@
+export default function location(state = "Seattle, WA", action) {
+    switch (action.type) {
+      case "CHANGE_LOCATION":
+        return action.payload;
+      default:
+        return state;
+    }
+  }
+
+// this reducer will be called at every action that's why default return is state 
+// it needs to have a state and action 
