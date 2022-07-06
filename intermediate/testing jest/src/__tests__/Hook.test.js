9982c3f20d7d1367e27ca5ca68c470e93f355dd6
nitishsaini706 <nitishsaini706@gmail.com>
2022-07-06 13:33:03 +0530

    addef mock fetch

--- intermediate/testing jest/src/__tests__/Hook.test.js
+++ intermediate/testing jest/src/__tests__/Hook.test.js
@@ -6,11 +6,27 @@
  import { renderHook} from "@testing-library/react-hooks";
  import useBreedList from "../useBreedList";
 
- test("gives an empty list with no animal", async () => {
-    const { result } = renderHook(() => useBreedList(""));
+ test("gives back breeds with an animal", async () => {
+    const breeds = [
+      "Havanese",
+      "Bichon Frise",
+      "Poodle",
+      "Maltese",
+      "Golden Retriever",
+      "Labrador",
+      "Husky",
+    ];
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        animal: "dog",
+        breeds,
+      })
+    );
+    const { result, waitForNextUpdate } = renderHook(() => useBreedList("dog"));
+  
+    await waitForNextUpdate();
   
     const [breedList, status] = result.current;
-  
-    expect(breedList).toHaveLength(0);
-    expect(status).toBe("unloaded");
+    expect(status).toBe("loaded");
+    expect(breedList).toEqual(breeds);
   });
